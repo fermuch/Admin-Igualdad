2c05bf6442097778f4ceaea77d1b2d8180345e21
Fernando Mumbach <fermuch@gmail.com>
2011-08-26 09:53:13 -0300

    preparando edición de usuarios

--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -1,4 +1,54 @@
 <?$this->load->view('admin/head',$title);?>
-<center><?=$table?></center>
+<script src="<?=base_url();?>assets/js/jquery-1.5.1.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+Ext.require([
+  'Ext.form.*',
+  'Ext.window.*'
+]);
+
+Ext.onReady(function (){
+  var form = Ext.widget('form', {
+    frame: true,
+    fieldDefaults: {
+      labelAlign: 'left',
+      msgTarget: 'none',
+      invalidCls: ''
+    },
+    defaults: {
+      anchor: '100%'
+    },
+    items: [{
+      xtype: 'textfield',
+      name: 'password',
+      inputType: 'password',
+      fieldLabel: 'Nueva Contraseña',
+      allowBlank: false,
+      minLength: 4
+    }]
+  });
+  
+  var win;
+  win = Ext.create('widget.window', {
+    title: 'Modificar Contraseña',
+    width: 350,
+    height: 'auto',
+    closable: true,
+    items: [form]
+  });
+  
+  $('.clickable').click(function (){
+    win.show();
+  });
+});
+</script>
+
+<center>
+<?php 
+ foreach ($table as $key):
+  echo "<a href=\"#\" class=\"clickable\" id=\"user_".$key['id']."\">".$key['username']."</a>";
+ endforeach;
+?>
+</center>
 
 <?$this->load->view('admin/foot');?>
